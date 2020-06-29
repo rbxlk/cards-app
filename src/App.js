63ad2782d8bf598556baa3d69d1803e1062eff97
rbxlk <rbxlk@protonmail.com>
2020-06-29 10:25:54 +0100

    Modify App to include Card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Card from './Card'
 
 class App extends React.Component{
 
   render() {
-    return <div className="header">{this.props.title}</div>
-    }
+    return (
+      <div className="header">{this.props.title}
+        <Card />
+      </div>
+    );
   }
+}
 
 export default App;

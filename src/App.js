25967e02b2a192afa1a1779f6b9cd08405a19e33
rbxlk <rbxlk@protonmail.com>
2020-06-29 10:36:11 +0100

    Add App sass file and update import statement for component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Card from './Card'
 
 class App extends React.Component{
 
   render() {
     return (
-      <div className="header">{this.props.title}
-        <Card />
+      <div>
+        <div className="header">{this.props.title} </div>
+          <Card />
       </div>
     );
   }

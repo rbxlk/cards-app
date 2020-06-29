0f382c67b5bb519b32c6f84158254e550135ff12
rbxlk <rbxlk@protonmail.com>
2020-06-29 10:10:47 +0100

    Modify App component from function to class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const App = ({ title }) => (
-  <div className="header">{title}</div>
-);
+class App extends React.Component{
+
+  render() {
+    return <div className="header">{this.props.title}</div>
+    }
+  }
 
 export default App;
